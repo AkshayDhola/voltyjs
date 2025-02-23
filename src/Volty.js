b874a65164d27a67bd601d64ec82315863cfe96b
Akshay <171118842+AkshayDhola@users.noreply.github.com>
2025-02-23 13:07:42 +0530

    fix issue of cuberto

--- src/Volty.js
+++ src/Volty.js
@@ -3,17 +3,14 @@ import { gsap } from "gsap";
 import "./Volty.css";
 
 export const Cuberto = ({
-  size = 40,
-  color = "transparent",
+  size = 10,
+  color = "#0C0C0C",
   smooth = false,
   text ="",
   radial=false,
-  img = ""
 }) => {
   const [mouse, setMouse] = useState({ x: 0, y: 0 });
   const cubertoRef = useRef(null);
-  const cubertoText = useRef(null);
-  const cubertoImg = useRef(null);
 
   useEffect(() => {
     const mouseMove = (event) => {
@@ -34,32 +31,18 @@ export const Cuberto = ({
         ease: smooth ? "power3" : "expo.out",
       });
     }
-    if (cubertoText.current) {
-      gsap.to(cubertoText.current, {
-        top: mouse.y,
-        left: mouse.x,
-        duration: 1,
-        ease: smooth ? "power3" : "expo.out",
-      });
-    }
-    if (cubertoImg.current) {
-      gsap.to(cubertoImg.current, {
-        top: mouse.y,
-        left: mouse.x,
-        duration: 1,
-        ease: smooth ? "power3" : "expo.out",
-      });
-    }
   }, [mouse]);
   return (
-    <>
-    {text ?(
-      <div className="cuberto_text" ref={cubertoText} style={{color:color}}>{text}</div>
-    ) : img ? (
-      <img className="cuberto_img" ref={cubertoImg} src={img} style={{width:size,height:size}}></img>
-    ):(
-      <div ref={cubertoRef} className={`cuberto ${radial && "radial"}`} style={{width:size,height:size,backgroundColor:color,border: color === 'transparent' && '1px solid rgba(0, 0,0,0.5)'}}></div>
-    )}
-    </>
+    <div ref={cubertoRef} className={`${text ? "cuberto_text":'cuberto'} ${radial ? "radial": ''}`} 
+    style={{
+      width:size,
+      height:size,
+      fontSize:size,
+      backgroundColor: text ? "" : color,
+      color : text ? color : "#0C0C0C",
+      border: color === '' && '1px solid #0C0C0C'
+    }}>
+      {text ? text : null}
+    </div>
   );
 };
