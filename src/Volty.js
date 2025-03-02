d87d29ce8446184cc7ba0522897d81890bbeac6c
AkshayDhola <akshaydhola3527@gmail.com>
2025-03-02 12:20:29 +0530

    add thereeD slider

--- src/Volty.js
+++ src/Volty.js
@@ -85,7 +85,7 @@ export const TextBentoo = ({ text = "Hello", color = "#0C0C0C", min = 50,max=300
   );
 };
 
-export const ImageTrail = ({images =[ "https://i.pinimg.com/474x/22/28/03/22280349e233c5c0773b895c997c2c04.jpg","https://i.pinimg.com/474x/44/92/b4/4492b4ba8a0e9539d073fc4bf5da8935.jpg"," https://i.pinimg.com/474x/87/df/b9/87dfb9718758e67702d9431bc3dec264.jpg","https://i.pinimg.com/736x/32/ae/37/32ae372a87ef780a789a754450ced028.jpg"," https://i.pinimg.com/736x/8a/2c/f9/8a2cf9dc06db2522e3da27295bcf155c.jpg"],width=108,height=216,rounded=5}) => {
+export const Trail = ({images =[ "https://i.pinimg.com/474x/22/28/03/22280349e233c5c0773b895c997c2c04.jpg","https://i.pinimg.com/474x/44/92/b4/4492b4ba8a0e9539d073fc4bf5da8935.jpg"," https://i.pinimg.com/474x/87/df/b9/87dfb9718758e67702d9431bc3dec264.jpg","https://i.pinimg.com/736x/32/ae/37/32ae372a87ef780a789a754450ced028.jpg"," https://i.pinimg.com/736x/8a/2c/f9/8a2cf9dc06db2522e3da27295bcf155c.jpg"],width=108,height=216,rounded=5}) => {
   const [mouse, setMouse] = useState({ x: 0, y: 0 });
   const trailRef = useRef(null);
 
@@ -99,7 +99,7 @@ export const ImageTrail = ({images =[ "https://i.pinimg.com/474x/22/28/03/222803
     };
   }, []);
   let zIndex=[];
-  for(let i=images.length+1;i>0;i--) zIndex.push(i*10);
+  for(let i=images.length+1;i>0;i--) zIndex.push(i*1000);
   useLayoutEffect(() => {
     if (trailRef.current) {
       gsap.to(trailRef.current.children, {
@@ -126,6 +126,47 @@ export const ImageTrail = ({images =[ "https://i.pinimg.com/474x/22/28/03/222803
             borderRadius: rounded,
           }}
         />
+      ))}
+    </div>
+  );
+};
+
+export const ThreeSlider =({images =[ "https://i.pinimg.com/474x/22/28/03/22280349e233c5c0773b895c997c2c04.jpg","https://i.pinimg.com/474x/44/92/b4/4492b4ba8a0e9539d073fc4bf5da8935.jpg"," https://i.pinimg.com/474x/87/df/b9/87dfb9718758e67702d9431bc3dec264.jpg","https://i.pinimg.com/736x/32/ae/37/32ae372a87ef780a789a754450ced028.jpg"," https://i.pinimg.com/736x/8a/2c/f9/8a2cf9dc06db2522e3da27295bcf155c.jpg"],size=400}) => {
+  const [card, setCard] = useState([]);
+  const sliderRef = useRef(null);
+  const scroll =()=>{
+    const position = window.scrollY;
+    const slider = sliderRef.current;
+    const transform=`translate3d(-50%,-50%,0) rotateX(0deg) rotateY(-25deg) rotateZ(-120deg)`;
+    const Zoff = position*0.5;
+    slider.style.transform = `${transform} translateY(${Zoff}px)`;
+  }
+  const MouseOver =(e)=>{
+    e.currentTarget.style.left = "15%";
+  }
+  const MouseOut =(e)=>{
+    e.currentTarget.style.left = "0%";
+  }
+
+
+  useEffect(() => {
+    const CardMove = images.map((image, index) => ({
+      i : index+1,
+      src:image,
+    }));
+    setCard(CardMove);
+    window.addEventListener("scroll", scroll);
+    return () => {
+      window.removeEventListener("scroll", scroll);
+    };
+  }, []);
+  
+  return (
+    <div className="threeslider" ref={sliderRef}>
+      {card.map((img) => (
+        <div key={img.i} className="card" style={{width:size,height:size}} onMouseOver={MouseOver} onMouseOut={MouseOut}>
+          <img src={img.src}/>
+        </div>   
       ))}
     </div>
   );
